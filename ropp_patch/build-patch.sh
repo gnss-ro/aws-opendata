a93dc93d5789c3faf8acb45330e49b117d7bdb7e
Sara Vannah <sara.vannah@veriskgov.com>
2024-04-01 14:40:19 -0400

    fix make instructions for build-patch

--- ropp_patch/build-patch.sh
+++ ropp_patch/build-patch.sh
@@ -8,25 +8,27 @@ cp -a patch/ropp_io ropp-11.0
 cp -a patch/ropp_pp  ropp-11.0
 
 #  Rebuild ropp_io with patch
-cd ropp-11.0//ropp_io
+cd ropp-11.0/ropp_io
 # Redo automakes 
 aclocal -I m4 --force
 automake -a -c
 autoconf
 echo "Rebuilding ropp_io..."
 # Run actual build 
-./configure #--prefix=${1-$ROPP_ROOT/gfortran} 
+./configure --prefix=${1-$ROPP_ROOT/gfortran} 
+make clean
 make 
 make install
-echo "ropp_io rebuild complete."
+echo "ropp_io rebuild complete. Check above for errors."
 
 # Rebuild ropp_pp
-cd ropp-11.0//ropp_pp 
+cd ropp-11.0/ropp_pp 
 aclocal -I m4 --force
 automake -a -c
 autoconf
 echo "Rebuilding ropp_pp..."
 ./configure 
+make clean
 make 
 make install
-echo "ropp_pp rebuild complete."
+echo "ropp_pp rebuild complete. Check above for errors"
