3ac52b628928b8759fdcec9dfec685ebe536f2e0
Sara Vannah <sara.vannah@veriskgov.com>
2024-04-04 17:42:46 -0400

    force copy to correct location

--- ropp_patch/docker-build-patch.sh
+++ ropp_patch/docker-build-patch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export FFLAGS-"-I/-I${ROPP_ROOT}/gfortran/include -I/usr/include -I/opt/include -g"
+
 ######################################################
 #    Point ROPP to data file locations
 ######################################################
 
 # Use included EGM96 geoid, WGS84 ellipsoid, and MSIS files
-cp  $ROPP_ROOT/ropp_pp/data/MSIS_coeff.nc ./MSIS_coeff.nc
+cp  $ROPP_ROOT/ropp_pp/data/MSIS_coeff.nc $ROPP_ROOT/MSIS_coeff.nc
 export GEOPOT_COEF=$ROPP_ROOT/ropp_pp/data/egm96.dat
 export GEOPOT_CORR=$ROPP_ROOT/ropp_pp/data/corrcoeff.dat
 
@@ -19,18 +21,18 @@ tar -xzvf patch.tar.gz
 #  Build ropp_io
 cp -ra ./patch/ropp_io/. $ROPP_ROOT/ropp_io/
 # Redo automakes to include aws2ropp
-cd /app/ropp/ropp-11.0/ropp_io && aclocal -I m4 --force
-cd /app/ropp/ropp-11.0/ropp_io && automake -a -c
-cd /app/ropp/ropp-11.0/ropp_io && autoconf
-cd /app/ropp/ropp-11.0/ropp_io && ./configure --prefix=${1-$ROPP_ROOT/gfortran} --without-sofa
-cd /app/ropp/ropp-11.0/ropp_io && make && make install
+cd $ROPP_ROOT/ropp_io && aclocal -I m4 --force
+cd $ROPP_ROOT/ropp_io && automake -a -c
+cd $ROPP_ROOT/ropp_io && autoconf
+cd $ROPP_ROOT/ropp_io && ./configure --prefix=${1-$ROPP_ROOT/gfortran} --without-sofa
+cd $ROPP_ROOT/ropp_io && make && make install
 
 cp -ra ./patch/ropp_pp/. $ROPP_ROOT/ropp_pp/
-cd /app/ropp/ropp-11.0/ropp_pp && aclocal -I m4 --force
-cd /app/ropp/ropp-11.0/ropp_pp && automake -a -c
-cd /app/ropp/ropp-11.0/ropp_pp && autoconf
-cd /app/ropp/ropp-11.0/ropp_pp && ./configure --without-sofa
-cd /app/ropp/ropp-11.0/ropp_pp && make && make install
+cd $ROPP_ROOT/ropp_pp && aclocal -I m4 --force
+cd $ROPP_ROOT/ropp_pp && automake -a -c
+cd $ROPP_ROOT/ropp_pp && autoconf
+cd $ROPP_ROOT/ropp_pp && ./configure --without-sofa
+cd $ROPP_ROOT/ropp_pp && make && make install
 
 # Return home 
 cd /mnt
